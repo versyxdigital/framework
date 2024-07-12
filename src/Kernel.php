1ecb0435e926eedf3292006ba71f59a38e318eae
Chris Rowles <c.rowles@sentrybay.com>
2024-07-12 04:32:01 +0100

    docs

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,13 +9,20 @@ use Versyx\Resolver;
 /**
  * Kernel class responsible for request dispatching and handling in the application.
  *
- * The kernel handles the request-response cycle. It initializes the application,
- * handles routing, executes controllers, and emits the final HTTP response.
+ * The kernel class handles the request-response cycle. It initializes the application,
+ * handles routing, executes route method handlers, and emits the final HTTP response.
  */
 class Kernel
 {
     /**
      * Dispatch the application request-response cycle.
+     * 
+     * This method receives the incoming HTTP request and creates a new server request
+     * object containing additional data, it then dispatches the router to handle the
+     * request and uses the resolver to respond.
+     * 
+     * The resolver will automatically resolve and inject any dependencies defined on
+     * route handler methods, before handling the request and returning the response.
      *
      * @param Container $app
      * @return void
